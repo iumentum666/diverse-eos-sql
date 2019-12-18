9ba9313dcaf1becaf0084dcf9565f35ca1bbeb6a
Alexander Nygård <alexander.nygard@eltelnetworks.no>
2019-12-18 06:54:24 +0000

    Update Sjekk_av_MM_MESSAGE_OUT_tabell.sql

--- Sjekk_av_MM_MESSAGE_OUT_tabell.sql
+++ Sjekk_av_MM_MESSAGE_OUT_tabell.sql
@@ -1,2 +1,5 @@
-SELECT *  FROM MM_MESSAGE_OUT t WHERE t.device_sequence IN (SELECT  p.sequence FROM PERSON_MOBILE p
-                       WHERE  p.last_sync_dttm < CONVERT(DATETIME, CONVERT(DATE, GETDATE())) -14)
+SELECT *  
+FROM MM_MESSAGE_OUT t 
+WHERE t.device_sequence 
+IN (SELECT  p.sequence FROM PERSON_MOBILE p
+WHERE  p.last_sync_dttm < CONVERT(DATETIME, CONVERT(DATE, GETDATE())) -14)

462f5a27c898418cbd420ce5dbe7e4690925426a
Alexander Nygård <alexander.nygard@eltelnetworks.no>
2020-06-03 15:38:33 +0200

    Ferdig.

--- Sjekk av travel på alle.sql
+++ Sjekk av travel på alle.sql
@@ -9,10 +9,16 @@ SELECT p.npu_id as PartUsageId,
 	  re.master_model_id as Hovedprodukt,
 	  r.place_id as TTP,
 	  r.zippost as Postnummer,
+	  a.county_id as Fylkesnummer,
+	  a.user_def13 as Prisregion,
 	  r.sales_grp_d as Avdelingsnummer
 
 
   FROM [IFSFSMPROD].[dbo].[non_part_usage] p
   LEFT JOIN [IFSFSMPROD].[dbo].[request] r on p.request_id = r.request_id
   LEFT JOIN [IFSFSMPROD].[dbo].[request_ext] re on p.request_id = re.request_id
-  where line_code = 'TRAVEL'
+
+  INNER JOIN place_address pa ON r.place_id = pa.place_id AND pa.address_type = 'DEFAULT'
+  LEFT JOIN address a ON pa.address_id = a.address_id
+
+  where p.line_code = 'TRAVEL'

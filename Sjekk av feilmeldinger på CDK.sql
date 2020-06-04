c19731d69feda50c4abfedcef6526257c5964f4e
Alexander Nygård <alexander.nygard@eltelnetworks.no>
2020-06-04 09:44:45 +0200

    La til Annulert av Telenor OK

--- Sjekk av feilmeldinger på CDK.sql
+++ Sjekk av feilmeldinger på CDK.sql
@@ -6,6 +6,7 @@ SELECT a.county_id                               AS 'Fylke',
        r.user_def17                              AS 'Ordre eier',
        r.cross_reference                         AS 'Ekstern ID',
        r.user_def2                               AS 'TN Status',
+
        l.note                                    AS 'Melding',
        CONVERT(DATETIME2(0), l.log_created_dttm) AS 'Timestamp'
 
@@ -13,23 +14,26 @@ FROM request r
 
      CROSS APPLY (SELECT
                        TOP 1
-           external_status,
-                      note,
-          log_created_dttm,
-           last_event_dttm,
+           external_status
+                         ,
+                      note
+                         ,
+          log_created_dttm
+                         ,
+           last_event_dttm
+                         ,
                   sequence
 
                       FROM mwf_log mw
                      WHERE mw.request_id = r.request_id
                        AND note NOT in
-                           ('OK - Status Changed', 'OK', 'Annulert av Telenor')
+                           ('OK - Status Changed', 'OK', 'Annulert av Telenor', 'Annulert av Telenor OK')
 
 					   AND note NOT LIKE ('% from state Closed')
 
                        AND note NOT LIKE 'Oppdatert av EOS OPS %'
 						and in_out = 'IN'
                      ORDER BY mw.sequence DESC) l
-
        INNER JOIN place_address pa ON r.place_id = pa.place_id AND pa.address_type = 'DEFAULT'
        LEFT JOIN address a ON pa.address_id = a.address_id
 

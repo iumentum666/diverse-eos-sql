449402a46411eaedae7c1b8edcf828fb63086d47
Nygård Alexander <alexander.nygard@eltelnetworks.no>
2020-06-10 11:58:15 +0000

    Update Sjekk av feilmeldinger på CDK.sql

--- Sjekk av feilmeldinger på CDK.sql
+++ Sjekk av feilmeldinger på CDK.sql
@@ -12,29 +12,23 @@ SELECT a.county_id                               AS 'Fylke',
 
 FROM request r
 
-     CROSS APPLY (SELECT
-                       TOP 1
-           external_status
-                         ,
-                      note
-                         ,
-          log_created_dttm
-                         ,
-           last_event_dttm
-                         ,
-                  sequence
+     CROSS APPLY (SELECT TOP 1
+           external_status,
+           note,
+           log_created_dttm,
+           last_event_dttm,
+		   in_out,
+           sequence
 
                       FROM mwf_log mw
                      WHERE mw.request_id = r.request_id
-                       AND note NOT in
-                           ('OK - Status Changed', 'OK', 'Annulert av Telenor', 'Annulert av Telenor OK')
 
-					   AND note NOT LIKE ('% from state Closed')
-
-                       AND note NOT LIKE 'Oppdatert av EOS OPS %'
-						and in_out = 'IN'
                      ORDER BY mw.sequence DESC) l
        INNER JOIN place_address pa ON r.place_id = pa.place_id AND pa.address_type = 'DEFAULT'
        LEFT JOIN address a ON pa.address_id = a.address_id
 
- WHERE r.req_class in ('CRI', 'NRI', 'WRI', 'OPN')
+ WHERE r.req_class in ('CRI', 'NRI', 'WRI', 'OPN', 'CLS')
+ AND l.note NOT in ('OK - Status Changed', 'OK', 'Annulert av Telenor', 'Annulert av Telenor OK')
+ AND l.note NOT LIKE ('% from state Closed')
+ AND l.note NOT LIKE 'Oppdatert av EOS OPS %'
+ AND l.in_out = 'IN'

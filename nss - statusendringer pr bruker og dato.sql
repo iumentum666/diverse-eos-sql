4323e6156c579f23105a68f2e573a690072753d4
anygard <Bombe008>
2022-07-01 13:57:48 +0200

    NSS - nye queries for de

--- nss - statusendringer pr bruker og dato.sql
+++ nss - statusendringer pr bruker og dato.sql
@@ -1,4 +1,5 @@
 SELECT
+r.request_id as 'Ordrenummer',
 CAST(re.event_dttm as date) as 'Status endret',
 r.req_class as 'Ordrertype',
 r.sales_grp_d as 'Avdeling på ordre',
@@ -6,24 +7,45 @@ re.created_by as 'Person sendt KA',
 p.first_name as 'Fornavn',
 p.last_name as 'Etternavn',
 p.user_def12 as 'Avdeling på person',
-
+re.note,
 CASE
-	WHEN re.note like '%KA%'  THEN 'KA'
-	WHEN re.note like '%ER%'  THEN 'ER'
-	WHEN re.note like '%EL%'  THEN 'EL'
-	WHEN re.note like '%APP%'  THEN 'APP'
-	WHEN re.note like '%NCC%'  THEN 'NCC'
-	WHEN re.note like '%CAN%'  THEN 'CAN'
-	WHEN re.note like '%EXE%'  THEN 'EXE'
-	WHEN re.note like '%OK%'  THEN 'OK'
-	WHEN re.note like '%VK%'  THEN 'VK'
-	WHEN re.note like '%UA%'  THEN 'UA'
-	WHEN re.note like '%CPO%'  THEN 'CPO'
-	WHEN re.note like '%PCR%'  THEN 'PCR'
-	WHEN re.note like '%UF%'  THEN 'UF'
-	WHEN re.note like '%TD%'  THEN 'TD'
-	WHEN re.note like '%RT%'  THEN 'RT'
-	WHEN re.note like '%MO%'  THEN 'MO'
+	WHEN left(re.note, 24) like '%KA%'  THEN 'KA'
+	WHEN left(re.note, 24) like '%ER%'  THEN 'ER'
+	WHEN left(re.note, 24) like '%EL%'  THEN 'EL'
+	WHEN left(re.note, 24) like '%APP%'  THEN 'APP'
+	WHEN left(re.note, 24) like '%NCC%'  THEN 'NCC'
+	WHEN left(re.note, 24) like '%CAN%'  THEN 'CAN'
+	WHEN left(re.note, 24) like '%EXE%'  THEN 'EXE'
+	WHEN left(re.note, 24) like '%OK%'  THEN 'OK'
+	WHEN left(re.note, 24) like '%VK%'  THEN 'VK'
+	WHEN left(re.note, 24) like '%UA%'  THEN 'UA'
+	WHEN left(re.note, 24) like '%CPO%'  THEN 'CPO'
+	WHEN left(re.note, 24) like '%PCR%'  THEN 'PCR'
+	WHEN left(re.note, 24) like '%UF%'  THEN 'UF'
+	WHEN left(re.note, 24) like '%TD%'  THEN 'TD'
+	WHEN left(re.note, 24) like '%RT%'  THEN 'RT'
+	WHEN left(re.note, 24) like '%MO%'  THEN 'MO'
+	WHEN left(re.note, 24) like '%RU%'  THEN 'RU'
+	WHEN left(re.note, 24) like '%RR%'  THEN 'RR'
+	WHEN left(re.note, 24) like '%RM%'  THEN 'RM'
+	WHEN left(re.note, 24) like '%TRD%'  THEN 'TRD'
+	WHEN left(re.note, 24) like '%PCS%'  THEN 'PCS'
+	WHEN left(re.note, 24) like '%PCC%'  THEN 'PCC'
+	WHEN left(re.note, 24) like '%MM%'  THEN 'MM'
+	WHEN left(re.note, 24) like '%VD%'  THEN 'VD'
+	WHEN left(re.note, 24) like '%ASS%'  THEN 'ASS'
+	WHEN left(re.note, 24) like '%ENP%'  THEN 'ENP'
+	WHEN left(re.note, 24) like '%IPO%'  THEN 'IPO'
+	WHEN left(re.note, 24) like '%IU%'  THEN 'IU'
+	WHEN left(re.note, 24) like '%PCF%'  THEN 'PCF'
+	WHEN left(re.note, 24) like '%PFC%'  THEN 'PFC'
+	WHEN left(re.note, 24) like '%WST%'  THEN 'WST'
+	WHEN left(re.note, 24) like '%TNC%'  THEN 'TNC'
+	WHEN left(re.note, 24) like '% TU %'  THEN 'TU'
+	WHEN left(re.note, 24) like '%UV%'  THEN 'UV'
+	WHEN left(re.note, 24) like 'Status endret til  av%' THEN 'Blank status'
+	WHEN left(re.note, 24) like 'Status endret til  og%' THEN 'Blank status'
+	WHEN left(re.note, 24) like 'Status endret til Full%' THEN 'Fullført'
 	ELSE re.note END as 'Status sendt'
 
 from request_event re

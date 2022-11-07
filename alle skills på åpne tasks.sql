7c38522f922105ce34f2c7fa5ebfdf734d04ca57
Alexander Nygård <alexander.nygard@eltelnetworks.no>
2022-11-07 12:00:41 +0100

    enda en rapport

--- alle skills på åpne tasks.sql
+++ alle skills på åpne tasks.sql
@@ -7,3 +7,22 @@ from task t
 
 left join task_skill ts on ts.task_id = t.task_id
 where t.task_status = 'Open'
+
+
+---
+-- Denne under finner alle skills på avdelinger
+SELECT
+r.sales_grp_d,
+t.task_template_id,
+ts.skill
+
+
+from task t
+left join request r on t.request_id = r.request_id
+
+left join task_skill ts on ts.task_id = t.task_id
+where t.task_status = 'Open' and t.created_dttm > '2022-08-01'
+and t.task_template_id not in ('INVOICING', 'NSS_BOOKING')
+
+group by r.sales_grp_d, t.task_template_id, ts.skill
+order by sales_grp_d, task_template_id
--- finne_manglende_vedlegg.sql
+++ finne_manglende_vedlegg.sql
@@ -11,3 +11,23 @@ inner join task t on r.request_id = t.request_id
 left join task_attachment ta on t.task_id = ta.task_id AND ra.attachment_id = ta.attachment_id
 
 where r.req_status not in ('COMPLETE','CANCELED','CLOSED') and ta.attachment_id IS NULL
+
+
+
+WITH task_attachment
+	as (SELECT
+ra.attachment_id as ra_attachment_id,
+t.task_id as ttask_id
+
+from request r
+inner join request_attachment ra on r.request_id = ra.request_id
+inner join task t on r.request_id = t.request_id
+left join task_attachment ta on t.task_id = ta.task_id AND ra.attachment_id = ta.attachment_id
+
+where r.req_status not in ('COMPLETE','CANCELED','CLOSED') and ta.attachment_id IS NULL and r.request_id = '65305')
+
+UPDATE task_attachment
+SET
+	task_id = ttask_id,
+	attachment_id = ra_attachment_id,
+	user_def2 = 'Y'

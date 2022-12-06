d4cd24b03d61c5a3599d4703334a8f77c597e716
Alexander Nygård <alexander.nygard@eltelnetworks.no>
2022-12-06 12:33:09 +0100

    Manglende vedlegg SQLen fungerer nå som forventet :-D

--- finne_manglende_vedlegg.sql
+++ finne_manglende_vedlegg.sql
@@ -12,22 +12,22 @@ left join task_attachment ta on t.task_id = ta.task_id AND ra.attachment_id = ta
 
 where r.req_status not in ('COMPLETE','CANCELED','CLOSED') and ta.attachment_id IS NULL
 
+-- QUERY under finner alle disse manglende vedleggene som en CTE - ob tar deretter en insert på tabellen for å oppdatere
 
+WITH manglende_vedlegg AS (
+	SELECT
+	r.request_id as 'Reqnr',
+	ra.attachment_id as 'ReqAttach',
+	t.task_id as 'TaskID'
 
-WITH task_attachment
-	as (SELECT
-ra.attachment_id as ra_attachment_id,
-t.task_id as ttask_id
+	from request r
+	inner join request_attachment ra on r.request_id = ra.request_id
+	inner join task t on r.request_id = t.request_id
+	left join task_attachment ta on t.task_id = ta.task_id AND ra.attachment_id = ta.attachment_id
 
-from request r
-inner join request_attachment ra on r.request_id = ra.request_id
-inner join task t on r.request_id = t.request_id
-left join task_attachment ta on t.task_id = ta.task_id AND ra.attachment_id = ta.attachment_id
+	where r.req_status not in ('COMPLETE','CANCELED','CLOSED') and ta.attachment_id IS NULL
+)
 
-where r.req_status not in ('COMPLETE','CANCELED','CLOSED') and ta.attachment_id IS NULL and r.request_id = '65305')
+INSERT INTO task_attachment (task_id, attachment_id)
 
-UPDATE task_attachment
-SET
-	task_id = ttask_id,
-	attachment_id = ra_attachment_id,
-	user_def2 = 'Y'
+SELECT TaskID, ReqAttach from manglende_vedlegg

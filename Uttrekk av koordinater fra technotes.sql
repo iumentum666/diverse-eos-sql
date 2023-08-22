17c9eb5bf58f7ed676e99e73a1a436573023f11f
Alexander Nygård <alexander.nygard@eltelnetworks.no>
2023-08-22 11:27:26 +0200

    Bedre variant. Feiler ikke...

--- Uttrekk av koordinater fra technotes.sql
+++ Uttrekk av koordinater fra technotes.sql
@@ -6,12 +6,12 @@ CONVERT(DATETIME2(0),r.created_dttm)	as 'Opprettet',
 SUBSTRING(rt.text, PATINDEX('%Koordinater%', rt.text)+13, 23) as Koordinater,
 
 CONCAT('https://maps.google.com/?q=',
-CAST(SUBSTRING(rt.text, PATINDEX('%Koordinater%', rt.text)+15, 2) AS numeric) + (CAST(SUBSTRING(rt.text, PATINDEX('%Koordinater%', rt.text)+18, 2) AS numeric) / 60) + ((CAST(SUBSTRING(rt.text, PATINDEX('%Koordinater%', rt.text)+21, 2) AS numeric) / 60) / 60),
+TRY_CAST(SUBSTRING(rt.text, PATINDEX('%Koordinater%', rt.text)+15, 2) AS numeric) + (TRY_CAST(SUBSTRING(rt.text, PATINDEX('%Koordinater%', rt.text)+18, 2) AS numeric) / 60) + ((TRY_CAST(SUBSTRING(rt.text, PATINDEX('%Koordinater%', rt.text)+21, 2) AS numeric) / 60) / 60),
 ',',
-CAST(SUBSTRING(rt.text, PATINDEX('%Koordinater%', rt.text)+26, 3) AS numeric) + (CAST(SUBSTRING(rt.text, PATINDEX('%Koordinater%', rt.text)+30, 2) AS numeric) / 60) + ((CAST(SUBSTRING(rt.text, PATINDEX('%Koordinater%', rt.text)+33, 2) AS numeric) / 60) / 60),
+TRY_CAST(SUBSTRING(rt.text, PATINDEX('%Koordinater%', rt.text)+26, 3) AS numeric) + (TRY_CAST(SUBSTRING(rt.text, PATINDEX('%Koordinater%', rt.text)+30, 2) AS numeric) / 60) + ((TRY_CAST(SUBSTRING(rt.text, PATINDEX('%Koordinater%', rt.text)+33, 2) AS numeric) / 60) / 60),
 '&z=16') as 'Googlelink'
 
 from request r
 left join request_text rt on r.request_id = rt.request_id
 
-  where r.created_dttm > '2021-03-01' and rt.text like '%Koordinater%' and rt.text_line_code = 'TECHNICALDATA' and r.req_status not in ('COMPLETE', 'CANCELED', 'CLOSED')
+  where r.created_dttm > '2023-01-01' and rt.text like '%Koordinater%' and rt.text_line_code = 'TECHNICALDATA' and r.req_status not in ('COMPLETE', 'CANCELED', 'CLOSED')
